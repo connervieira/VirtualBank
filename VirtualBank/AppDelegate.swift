10dbd262932f1730ffb5297067eafd8f23109680
Conner Vieira <vieirast@icloud.com>
2019-07-02 11:28:27 -0400

    Initial Commit

---
+++ VirtualBank/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VirtualBank
+//
+//  Created by Conner Vieira on 7/2/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VirtualBank/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VirtualBank
+//
+//  Created by Conner Vieira on 7/2/19.
+//  Copyright © 2019 V0LT. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
